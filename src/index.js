21693947ad035264be5f72bf96c06d6e757606e5
kerelape <kiryaev.developer@gmail.com>
2022-04-19 18:57:21 +0500

    Add filterEachEntryOf

--- src/index.js
+++ src/index.js
@@ -32,3 +32,24 @@ const mapEachEntryOf = (object, block) => {
         .forEach(([key, value]) => result[key] = value)
     return result
 }
+
+/**
+ * 
+ * @param {object} object 
+ * @param {(key: string, value: any) => boolean} block
+ * @returns {object} 
+ */
+const filterEachEntryOf = (object, block) => {
+    if (typeof object !== "object") {
+        throw new Error("can ony work with objects")
+    }
+    if (typeof block !== "function") {
+        throw new Error("block is not a function")
+    }
+    const result = {}
+    Object.keys(object)
+        .map(key => [key, value, block(key, value)])
+        .filter(([_, _, f]) => f)
+        .forEach(([key, value]) => result[key] = value)
+    return result
+}
