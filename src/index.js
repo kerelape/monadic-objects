c8c4cc318601016f49bb731af06f1b637f528bab
kerelape <kiryaev.developer@gmail.com>
2022-04-19 18:50:26 +0500

    Add mapEachEntryOf

--- src/index.js
+++ src/index.js
@@ -12,3 +12,23 @@ const forEachEntryOf = (object, block) => {
     Object.keys(object).forEach(key => block(String(key), object[key]))
     return { ...object }
 }
+
+/**
+ * 
+ * @param {object} object 
+ * @param {(key: string, value: any) => [key: string, value: any]} block 
+ * @returns {object} The modified object
+ */
+const mapEachEntryOf = (object, block) => {
+    if (typeof object !== "object") {
+        throw new Error("can only work with objects")
+    }
+    if (typeof block !== "function") {
+        throw new Error("block is not a function")
+    }
+    const result = {}
+    Object.keys(object)
+        .map(key => block(String(key), object[key]))
+        .forEach(([key, value]) => result[key] = value)
+    return result
+}
