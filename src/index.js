12dab2af16ddad0081eda1a3bff0964cf36f9c2d
kerelape <kiryaev.developer@gmail.com>
2022-04-19 18:39:28 +0500

    Add forEachEntryOf

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 
-console.log("Hello, World!")
+/**
+ * 
+ * @param {object} object 
+ * @param {(key: string, value: any) => void} block 
+ * @returns {object} Copy of the original object
+ */
+const forEachEntryOf = (object, block) => {
+    if (typeof block !== "function") {
+        throw new Error("block is not a function")
+    }
+    Object.keys(object).forEach(key => block(String(key), object[key]))
+    return { ...object }
+}
