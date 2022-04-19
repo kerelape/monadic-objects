bff16e4b7287d28624b0f108b2b0cc2151412dff
kerelape <kiryaev.developer@gmail.com>
2022-04-20 01:25:24 +0500

    Add map tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { ok, throws } = require("assert");
+const { ok, throws, deepEqual } = require("assert");
 const index = require("../src/index");
 
 describe("forEach", () => {
@@ -20,5 +20,33 @@ describe("forEach", () => {
         it("over simple object", () => {
             ok(() => ({ a: "a", b: "b" }).forEach(() => {}));
         });
+    });
+});
+
+describe("map", () => {
+    it("fails without 'block'", () => {
+        throws(() => ({}).map())
+    });
+    describe("fails with incorrect 'block'", () => {
+        [0, "", Symbol(), [], {}, undefined, false]
+            .forEach(block => {
+                it(`of type ${typeof block}`, () => {
+                    throws(() => ({}).map(block));
+                });
+            });
+    })
+    it("can map keys", () => {
+        deepEqual(
+            ({ a: "b", c: "c" })
+                .map((key, value) => [`new_${key}`, value]),
+            ({ new_a: "b", new_c: "c" })
+        );
+    });
+    it("can map values", () => {
+        deepEqual(
+            ({ a: "b", c: "c" })
+                .map((key, value) => [key, value + value]),
+            ({ a: "bb", c: "cc" })
+        );
     });
 });
